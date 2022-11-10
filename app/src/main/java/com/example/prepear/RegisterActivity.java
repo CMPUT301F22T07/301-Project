62fc22500d70206f7823b0999a0ce1bf2ea18ea2
Shihao Liu <lsh20011018@gmail.com>
2022-11-10 00:38:54 -0700

    start coding for RegisterActivity

--- app/src/main/java/com/example/prepear/RegisterActivity.java
+++ app/src/main/java/com/example/prepear/RegisterActivity.java
@@ -25,7 +25,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        // on below part:
         TextView loginText = findViewById(R.id.already_registered_text);
         loginText.setOnClickListener(new View.OnClickListener() {
             @Override
