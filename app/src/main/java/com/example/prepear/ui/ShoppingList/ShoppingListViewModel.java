e5a91d640989b7e601829c5e07b179e2234f9272
Jingyi Xu <jx15@ualberta.ca>
2022-11-23 01:11:40 -0700

    new db project

--- app/src/main/java/com/example/prepear/ui/ShoppingList/ShoppingListViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.prepear.ui.ShoppingList;
-
-import androidx.lifecycle.ViewModel;
-
-public class ShoppingListViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
